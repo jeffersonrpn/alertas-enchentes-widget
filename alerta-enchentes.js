76625ab52ad9bbe9a48aa07e217aef303e158bfb
Jefferson Neves <jefferson.rpn@gmail.com>
2016-08-24 03:14:08 -0300

    Load d3 library async

--- alerta-enchentes.js
+++ alerta-enchentes.js
@@ -1,5 +1,23 @@
 var Alert = (function(window, undefined) {
   var Alert = {};
+
+  function loadScript(url, callback) {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = url;
+
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+
+    script.onload = script.onreadystatechange = function() {
+      var rdyState = script.readyState;
+      if (!rdyState || /complete|loaded/.test(script.readyState)) {
+        callback();
+        script.onload = null;
+        script.onreadystatechange = null;
+      }
+    }
+  }
   function loadSupportingFiles(callback) {}
   function getWidgetParams() {}
   function getData(params, callback) {}
@@ -9,9 +27,11 @@ var Alert = (function(window, undefined) {
       "<div>" +
       "  <h3>Here is the widget</h3>" +
       "</div>";
+    var scg = d3.select("#alerta-enchentes").append("svg");
   }
-
-  drawWidget();
+  loadScript('//d3js.org/d3.v3.min.js', function() {
+    drawWidget();
+  })
 
   return Alert;
 })(window);
