ea7481f3dcd6a86b876967546c246f35ff372464
Jefferson Neves <jefferson.rpn@gmail.com>
2017-03-01 16:04:38 -0300

    Update chart

--- alerta-enchentes.js
+++ alerta-enchentes.js
@@ -1,6 +1,5 @@
 var Alert = (function(window, undefined) {
   var Alert = {};
-  Alert.$ = Alert.jQuery = jQuery.noConflict(true);
 
   function loadScript(url, callback) {
     var script = document.createElement('script');
@@ -81,7 +80,7 @@ var Alert = (function(window, undefined) {
   function getData(params, callback) {
     Alert.$.ajax({
       method: 'GET',
-      url: 'http://alertas-enchentes-api.herokuapp.com/station/'+params.station+'/prediction?timestamp='+Math.floor(params.timestamp/1000),
+      url: 'http://localhost:8080/station/'+params.station+'/prediction?timestamp='+Math.floor(params.timestamp/1000),
       data: {},
       success: function(river) {
         callback(river, params.timestamp, params.htmlWrapper);
@@ -134,34 +133,24 @@ var Alert = (function(window, undefined) {
 
     var d3Widget = d3;
 
-    var data = river.data;
-
     var margin = {
           top: 50,
           right: 10,
           bottom: 30,
-          left: 10
+          left: 30
         },
         width = 600 - margin.left - margin.right,
-        height = 160 - margin.top - margin.bottom,
+        height = 300 - margin.top - margin.bottom,
         viewBoxWidth = width + margin.left + margin.right,
         viewBoxHeight = height + margin.top + margin.bottom,
-        baseValue = margin.bottom,
-        tooltipWidth = 50,
-        tooltipHeight = 30;
+        baseValue = 0,
+        tooltipWidth = 125,
+        tooltipHeight = 30,
+        tooltipPadding = -20;
 
-    var x = d3Widget.scale.ordinal()
-        .rangeRoundBands([0, width], 0);
-    var y = d3Widget.scale.linear()
-        .range([height, 0]);
-    var xAxis = d3Widget.svg.axis()
-        .scale(x)
-        .orient("bottom");
-    var yAxis = d3Widget.svg.axis()
-        .scale(y)
-        .orient("left");
-    x.domain(data.map(function(d) { return d.timestamp; }));
-    y.domain([d3Widget.min(data, function(d) { return d.predicted; }), d3Widget.max(data, function(d) { return d.predicted; })]);
+    var bisectDate = d3Widget.bisector(function(d) { return d.timestamp; }).left;
+    var formatTimeLiteral = d3Widget.time.format("%Hh%M");
+    var formatDateTimeLiteral = d3Widget.time.format("%Hh%M");
 
     //Get alert info
     var alertTimestamp = getAlertTimestamp(river);
@@ -175,22 +164,21 @@ var Alert = (function(window, undefined) {
 
     var mapInfo = d3Widget.select("#"+htmlWrapper)
       .append("div")
-        .attr("class", "alerta-enchentes-map-info")
+        .attr("class", "alertas-enchentes-widget")
         .style({
-          "width": "100%",
           "padding": "20px",
-          "background-color": "rgba(11, 51, 65, 0.8)",
+          "background-color": "rgba(11, 51, 65, 0.95)",
           "font": "16px arial,sans-serif-light,sans-serif",
           "color": "#fff"
         });
       mapInfo.append("div")
-        .attr("class", "alerta-enchentes-map-time-info")
+        .attr("class", "alertas-enchentes-widget-title")
         .style({
           "font-size": "25px"
         })
-        .html(river.info.name);
+        .html(river.info.riverName+" em "+river.info.cityName);
       mapInfo.append("div")
-      .html(moment(timestamp).format('H:mm'));
+        .html("Previsões a partir de "+formatTimeLiteral(new Date(river.params.timestamp*1000)));
 
       // Alert info
       var alertInfo = mapInfo.append("div")
@@ -219,171 +207,293 @@ var Alert = (function(window, undefined) {
       alertInfoText.append("div")
         .html(alertTimestamp.description);
 
-    // Graph
+    // Chart
     if (river.data.length < 1) return;
 
     var graph = mapInfo.append("div")
-    .attr("class", "alerta-enchentes-graph");
+    .attr("class", "alerta-enchentes-chart");
     var svg = graph.append("svg")
         .attr("width", "100%")
         .attr("viewBox", "0 0 "+viewBoxWidth+" "+viewBoxHeight)
       .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var domainMin = d3Widget.min(data, function(d) { return d.predicted; });
-    var domainMax = d3Widget.max(data, function(d) { return d.predicted; });
-    if (domainMax < river.info.floodThreshold) {
-        var domainMax = river.info.floodThreshold;
-    }
-    x.domain(data.map(function(d) { return d.timestamp; }));
-    y.domain([domainMin, domainMax]);
+    var x = d3Widget.time.scale()
+        .range([0, width]);
+    var y = d3Widget.scale.linear()
+        .range([height, 0]);
+    var valuearea = d3Widget.svg.area()
+        .x(function(d) { return x(d.timestamp); })
+        .y1(function(d) { return y(d.measured); });
+    var valueline = d3Widget.svg.line()
+      .x(function(d) { return x(d.timestamp); })
+      .y(function(d) { return y(d.measured); });
+    var valueline2 = d3Widget.svg.line()
+      .x(function(d) { return x(d.timestamp); })
+      .y(function(d) { return y(d.predicted); });
+    var xAxis = d3Widget.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .ticks(12)
+        .tickFormat(d3.time.format("%Hh"));
+    var yAxis = d3Widget.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(12)
+        .tickFormat(function(d) {
+          return Math.round((d * 0.01) * 100) / 100
+        });
 
-    // Draw lines
-    svg.append("line")
+    var areaG = svg.append("g").attr("class", "areaG");
+    var linesG = svg.append("g").attr("class", "lines");
+
+    var areaSVG = areaG.append("path")
+      .attr("class", "alerta-enchentes-chart-area")
+      .style({
+        "fill": "#7baff0",
+        "fill-opacity": "0.3"
+      });
+    var line2SVG = areaG.append("path")
+      .attr("class", "alerta-enchentes-chart-line")
+      .style({
+        "fill": "none",
+        "stroke": "#1878F0",
+        "stroke-width": "2"
+      });
+
+    var axisSVG = areaG.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .attr("class", "alerta-enchentes-chart-x-axis")
+      .style({
+        "fill": "none",
+        "stroke": "#999"
+      });
+
+    var axisYSVG = areaG.append("g")
+      .attr("class", "alerta-enchentes-chart-y-axis")
+      .style({
+        "fill": "none",
+        "stroke": "#999"
+      });
+
+    var attentionLine = linesG.append("line")
+    .attr({
+      "x1": 0,
+      "x2": width,
+      "fill": "none",
+      "stroke-width": "2px",
+      "opacity": 1,
+      "stroke-dasharray": "10,5",
+      "stroke": color("ATENCAO")
+    });
+    var attentionText = linesG.append("text")
+    .attr({
+      "x": margin.right,
+      "fill": color("ATENCAO"),
+      "opacity": 1,
+      "font-size": "10",
+      "font-family": "sans"
+    })
+    .text("Nível de atenção");
+    var alertLine = linesG.append("line")
       .attr({
-        "x1": margin.right*2,
-        "y1": y(river.info.warningThreshold),
-        "x2": width-margin.left*2,
-        "y2": y(river.info.warningThreshold),
+        "x1": 0,
+        "x2": width,
         "fill": "none",
         "stroke-width": "2px",
-        "opacity": 0.5,
+        "opacity": 1,
         "stroke-dasharray": "10,5",
         "stroke": color("ALERTA")
       });
-    svg.append("text")
+    var alertText = linesG.append("text")
       .attr({
-        "x": margin.right*2,
-        "y": y(river.info.warningThreshold) + 12,
+        "x": margin.right,
         "fill": color("ALERTA"),
-        "opacity": 0.5,
+        "opacity": 1,
         "font-size": "10",
         "font-family": "sans"
       })
       .text("Nível de alerta");
-    svg.append("line")
+    var floodLine = linesG.append("line")
       .attr({
         "fill": "none",
-        "x1": margin.right*2,
-        "y1": y(river.info.floodThreshold),
-        "x2": width-margin.left*2,
-        "y2": y(river.info.floodThreshold),
+        "x1": 0,
+        "x2": width,
         "stroke-width": "2px",
-        "opacity": 0.5,
+        "opacity": 1,
         "stroke-dasharray": "10,5",
         "stroke": color("INUNDACAO")
       });
-    svg.append("text")
+    var floodText = linesG.append("text")
       .attr({
-        "x": margin.right*2,
-        "y": y(river.info.floodThreshold) - 4,
+        "x": margin.right,
         "fill": color("INUNDACAO"),
-        "opacity": 0.5,
+        "opacity": 1,
         "font-size": "10",
         "font-family": "sans"
       })
       .text("Nível de enchente");
-
-    var tooltip = svg.append("g")
-      .attr("class", "alert-tooltip")
-      .attr("opacity", 0);
-    var tooltipRect = tooltip.append("rect")
+    var predictionText = linesG.append("text")
       .attr({
-        "width": tooltipWidth,
-        "height": tooltipHeight,
-        "x": margin.left/2,
-        "y": margin.top/2,
-        "rx": 2,
-        "ry": 2,
-        "stroke": "#1878f0",
-        "stroke-width": 3,
-        "fill": "#1878f0",
-        "opacity": 1
-      });
-    var tooltipText = tooltip.append("text")
-      .attr({
-        "x": margin.left/2 + 14,
-        "y": margin.top/2 + 25,
-        "fill": "#fff",
-        "font-size": "8",
-        "font-family": "sans"
+        "fill": color("NORMAL"),
+        "opacity": 1,
+        "font-size": "10",
+        "font-family": "sans",
+        "text-anchor": "end"
       })
-      .text("");
-    var tooltipText2 = tooltip.append("text")
-      .attr({
-        "x": margin.left/2 + 10,
-        "y": margin.top/2 + 15,
-        "fill": "#fff",
-        "font-size": "12",
-        "font-family": "sans"
-      })
-      .text("");
+      .text("Previsão");
 
-      // svg.append("g")
-      //     .attr("class", "x axis")
-      //     .attr("transform", "translate(0," + height + ")")
-      //     .call(xAxis);
-
-      // svg.append("g")
-      //     .attr("class", "y axis")
-      //     .call(yAxis);
-
-      var bar = svg.selectAll(".bar")
-          .data(data);
-        // Render bars
-        bar.enter().append("rect")
-          .attr("x", function(d) { return x(d.timestamp); })
-          .attr("width", x.rangeBand())
-          .attr("y", function(d) { return y(d.predicted); })
-          .attr("height", function(d) { return height - y(d.predicted) + baseValue; })
-          .attr("fill", function(d) { return color(d.predictedStatus); })
-          .style("opacity", 0.4)
-          .on("mouseover", function(d) {
-            d3Widget.select(this).transition().duration(200).style("opacity", 1);
-
-            var date = new Date(d.timestamp*1000);
-            var hours = date.getHours();
-            var minutes = "0" + date.getMinutes();
-            var time = hours + ':' + minutes.substr(-2);
-            tooltipText.text(time);
-            tooltipText2.text(d.predicted);
-            tooltip.transition()
-                .duration(200)
-                .style("opacity", 1);
-            var positionX = x(d.timestamp) - tooltipWidth/2;
-            var positionY = y(d.predicted) - tooltipHeight - 40;
-            tooltip.attr("transform", "translate(" + positionX + "," + positionY + ")");
-            })
-        .on("mouseout", function(d) {
-          d3Widget.select(this).transition().duration(200).style("opacity", 0.4);
-          tooltip.transition()
-            .duration(500)
-            .style("opacity", 0);
+    var dots = svg.append("g")
+        .attr({
+          "class": "alerta-enchentes-chart-dots",
+          "opacity": 0,
+          "stroke-width": "2"
         });
-        // Render top bars
-        bar.enter().append("rect")
-          .attr("width", x.rangeBand())
-          .attr("x", function(d) { return x(d.timestamp); })
-          .attr("y", function(d) { return y(d.predicted); })
-          .attr("fill", function(d) { return color(d.predictedStatus); })
-          .attr("height", 2);
-        // Render time
-        bar.enter().append("text")
-          .attr("fill", "#fff")
-          .attr("font-size", "12px")
-          .attr("text-anchor", "start")
-          .attr("x", function(d) { return x(d.timestamp)+x.rangeBand()+4; })
-          .attr("y", height + baseValue - 5)
-          .text(function(d, i) {
-            var minutes = moment(d.timestamp*1000).format("mm");
-            if (minutes === "00" && i !== data.length-1) {
-              return moment(d.timestamp*1000).format("H:mm");
-            }
+    var rectMouse = svg.append("rect")
+        .attr("width", width)
+        .attr("height", height)
+        .style("fill", "none")
+        .style("pointer-events", "all");
+    var selectedValue = svg.append("g")
+        .attr("class", "alerta-enchentes-chart-selected-value")
+        .style("display", "none");
+    var selectedValueLine = selectedValue.append("line")
+        .style({
+          "stroke": "#1878F0",
+          "fill": "none",
+          "stroke-width": "2"
+        });
+    var selectedValueCircle = selectedValue.append("g");
+        selectedValueCircle.append("circle")
+          .attr("r", 8)
+          .style({
+            "fill": "#fff"
+          });
+        selectedValueCircle.append("circle")
+          .attr("r", 5)
+          .style({
+            "fill": "#fff",
+            "stroke":"#1878F0",
+            "stroke-width": "2"
+          });
+    var selectedValueRect = selectedValue.append("rect")
+        .attr("rx", 4)
+        .attr("ry", 4)
+        .attr("width", tooltipWidth)
+        .attr("height", tooltipHeight)
+        .style({
+          "fill": "#1878F0"
+        });
+    var selectedValueText = selectedValue.append("text")
+        .attr("x", 5)
+        .attr("dy", ".35em")
+        .attr("text-anchor", "middle")
+        .style({
+          "fill": "#fff",
+          "font-size": "12"
         });
 
-    function type(d) {
-      d.predicted = +d.predicted;
-      return d;
+    var data = [];
+    var data2 = [];
+    river.data.forEach(function(d) {
+      d.timestamp = new Date(d.timestamp*1000);
+      if (d.measured !== null) {
+        data.push(d);
+      }
+      if (d.predicted !== null) {
+        data2.push(d);
+      }
+    });
+
+    var domainMax = d3Widget.max(data, function(d) { return d.measured; });
+    var domainMin = d3Widget.min(data, function(d) { return d.measured; });
+    if (domainMax < river.info.floodThreshold) {
+      domainMax = river.info.floodThreshold;
+    }
+    if (domainMin > river.info.floodThreshold - 1000) {
+      domainMin = river.info.floodThreshold - 1000;
+    }
+    x.domain(d3.extent(river.data, function(d) { return d.timestamp; }));
+    y.domain([domainMin - 10, domainMax]);
+    valuearea.y0(y(domainMin));
+
+    attentionLine.attr({
+      "y1": y(river.info.attentionThreshold),
+      "y2": y(river.info.attentionThreshold)
+    });
+    attentionText.attr({
+      "y": y(river.info.attentionThreshold) - 4,
+    });
+    alertLine.attr({
+      "y1": y(river.info.warningThreshold),
+      "y2": y(river.info.warningThreshold)
+    });
+    alertText.attr({
+      "y": y(river.info.warningThreshold) - 4,
+    });
+    floodLine.attr({
+      "y1": y(river.info.floodThreshold),
+      "y2": y(river.info.floodThreshold),
+    });
+    floodText.attr({
+      "y": y(river.info.floodThreshold) - 4,
+    });
+    predictionText.attr({
+      "x": x(data2[data2.length-1].timestamp),
+      "y": y(data2[data2.length-1].predicted) - 4,
+    });
+
+    areaSVG.datum(data).attr("d", valuearea);
+    line2SVG.datum(data2).attr("d", valueline2);
+
+    axisSVG.call(xAxis);
+    axisYSVG.call(yAxis);
+    d3Widget.selectAll(".tick text").style({
+      "font-size": "10",
+      "font-family": "sans",
+      "fill": "#999"
+    });
+    d3Widget.selectAll(".tick").style({"fill" : "#999"});
+
+    rectMouse
+      .on("mouseover", mouseover)
+      .on("mouseout", mouseout)
+      .on("mousemove", mousemove);
+
+    function mouseover() {
+      selectedValue.style("display", null);
+    }
+
+    function mouseout() {
+      selectedValue.style("display", "none");
+    }
+
+    function mousemove() {
+      var
+        x0 = x.invert(d3Widget.mouse(this)[0]),
+        i = bisectDate(data2, x0, 1),
+        d0 = data2[i - 1],
+        d1 = data2[i],
+        d = x0 - d0.timestamp > d1.timestamp - x0 ? d1 : d0,
+        measured = Math.round((d.predicted * 0.01) * 100) / 100;
+      selectedValueCircle.attr("transform", "translate(" + x(d.timestamp) + "," + y(d.predicted) + ")");
+      selectedValueLine.attr({"x1": x(d.timestamp), "y1": (y(d.predicted)+tooltipPadding), "x2": x(d.timestamp), "y2": y(domainMin)});
+      selectedValueText.text(measured.toString().replace('.', ',')+"m em "+formatTimeLiteral(d.timestamp));
+
+      var xTooltip;
+      if (x(d.timestamp) > width - tooltipWidth/2 + 10) {
+        xTooltip = width - tooltipWidth/2 + 10;
+      } else if (x(d.timestamp) < tooltipWidth/2) {
+        xTooltip = tooltipWidth/2;
+      } else {
+        xTooltip = x(d.timestamp);
+      }
+
+      selectedValueText.attr("transform", "translate(" + xTooltip + "," + (y(d.predicted)-(tooltipHeight/2)+tooltipPadding) + ")");
+      selectedValueRect.attr({"x": (xTooltip-(tooltipWidth/2)), "y": (y(d.predicted)-tooltipHeight+tooltipPadding)});
+      d3Widget.select('.alert-tip').style("visible", "visible");
+      d3Widget.select('.alert-measure').text(measured.toString()+"m".replace('.', ','));
+      d3Widget.select('.alert-time').text(formatTimeLiteral(d.timestamp));
     }
 
     function color(predictedStatus) {
@@ -408,6 +518,7 @@ var Alert = (function(window, undefined) {
       loadScript('//cdnjs.cloudflare.com/ajax/libs/moment.js/2.14.1/moment.min.js', function() {
         var url = getScriptUrl();
         var params = getUrlParameters(url);
+        Alert.$ = Alert.jQuery = jQuery.noConflict(true);
         getWidgetLocation(drawWidget)
       });
     });
